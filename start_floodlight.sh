536842f6a1f23970ecea637a74a47ad6bd83eea0
Shyama Bhuvanendran <shyamabs1991@gmail.com>
2019-03-27 12:00:49 -0400

    Delete start_floodlight.sh

--- start_floodlight.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "starting floodlight controller in the background...."
-cd /home/mininet/floodlight/
-nohup sudo java -jar /home/mininet/floodlight/target/floodlight.jar
-echo "starting mininet 5 host 5 switch topology...."
-nohup sudo /home/mininet/src/myTopo.py
